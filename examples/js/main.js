e5eba21edeeacade6555cb4599f69bd1334d6142
Yannick Van Avermaet <yannick.van.avermaet@persgroep.be>
2015-01-06 16:13:53 +0100

    ADSF-36: DOMContentLoaded isn't fired in IE8.

--- examples/js/main.js
+++ examples/js/main.js
@@ -1,41 +1,42 @@
 'use strict';
 
 var App = {
-
-    config: {
-
-        formErrors: {
+	config: {
+		formErrors: {
 			empty: 'Dit veld mag niet leeg zijn',
 			unchecked: 'U moet dit veld aanvinken',
 			invalidEmail: 'Gelieve een bestaand email adres in te voeren'
 		}
-    },
-    modules: [],
-    views: {
-
-        dialog: []
-    }
+	},
+	modules: [],
+	views: {
+		dialog: []
+	}
 };
 
 App.main = (function() {
+	function _init() {
+		$.publish('APP/bootstrap');
+	}
 
-    function _init() {
+	function _load() {
+		$.publish('APP/load');
+	}
 
-        $.publish('APP/bootstrap');
-    }
-
-    function _load() {
-
-        $.publish('APP/load');
-    }
-
-    return {
-
-        init: _init,
-        load: _load
-    }
-
+	return {
+		init: _init,
+		load: _load
+	}
 }());
 
-$(document).on('DOMContentLoaded', App.main.init);
+// DOM loaded with IE fix:
+if (document.attachEvent){
+	document.attachEvent('onreadystatechange', function(){
+		if (document.readyState==='complete'){
+			App.main.init();
+		}
+	});
+}else{
+	$(document).on('DOMContentLoaded', App.main.init);
+}
 $(window).on('load', App.main.load);
